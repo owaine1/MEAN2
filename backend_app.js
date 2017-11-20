33ab3d0aec27d6d405289f6e6de523fdfc77cd69
Owain <owaine1@live.com>
2017-11-20 10:29:38 +0800

    first commit for MEAN2

---
+++ backend_app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var backend_app = express();
+var routes = require('./routes/main_routes');
+var bodyParser = require('body-parser');
+var port = 3500;
+
+backend_app.set('view engine', 'ejs');
+backend_app.use(bodyParser.urlencoded({
+    extended: true
+}));
+backend_app.use(bodyParser.json());
+backend_app.use(routes);
+
+backend_app.listen(port, listening);
+function listening() {
+    console.log('broadcasting on localhoast:' + port);
+}
